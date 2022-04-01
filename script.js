42cb897cf19492eae6468628da0e44edf444c88a
tarun123456789055555 <89084799+tarun123456789055555@users.noreply.github.com>
2022-04-01 16:02:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-// Declaring the variables
-let lon;
-let lat;
-let temperature = document.querySelector(".temp");
-let summary = document.querySelector(".summary");
-let loc = document.querySelector(".location");
-let icon = document.querySelector(".icon");
-const kelvin = 273;
-  
-window.addEventListener("load", () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      lon = position.coords.longitude;
-      lat = position.coords.latitude;
-  
-      // API ID
-      const api = "6d055e39ee237af35ca066f35474e9df";
-  
-      // API URL
-      const base =
-`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-`lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
-  
-      // Calling the API
-      fetch(base)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          temperature.textContent = 
-              Math.floor(data.main.temp - kelvin) + "°C";
-          summary.textContent = data.weather[0].description;
-          loc.textContent = data.name + "," + data.sys.country;
-          let icon1 = data.weather[0].icon;
-          icon.innerHTML = 
-              `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
-        });
+/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
+SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
+const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const inputVal = input.value;
+
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${
+        weather[0]["icon"]
+      }@2x.png`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city 😩";
     });
-  }
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
 });
